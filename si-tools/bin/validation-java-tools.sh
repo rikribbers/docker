f751d06befd77251b1490162d5caf21a2e53eff8
Rik Ribbers <rik.ribbers@gmail.com>
2017-06-01 10:57:23 +0200

    updatede tools with java-tools for validation

--- si-tools/bin/validation-java-tools.sh
+++ si-tools/bin/validation-java-tools.sh
@@ -1,61 +1,5 @@
 #!/bin/bash
 
-JAR_FILE=/opt/java-tools/en16931-xml-validator.jar
-
-usage()
-{
-    cat << EOF
-    usage: $0 options
-
-    OPTIONS:
-       -u      SI-UBL version to use (xsl version)
-       -f      file to be verified
-       -v      verbose output
-       -h      Show this message
-EOF
-}
-
-#defaults
-VERBOSE=0;
-SI_UBL_VERSION=
-MY_FILE=
-# :v --> : charater indicates a required field
-while getopts "hvu:f:" OPTION
-do
-    case $OPTION in
-        h)
-            usage
-            exit 1
-            ;;
-        v)
-            VERBOSE=1
-            ;;
-        f)
-            MY_FILE=$OPTARG
-            ;;
-        u)
-            SI_UBL_VERSION=$OPTARG
-            ;;
-        ?)
-            usage
-            exit
-            ;;
-    esac
-done
-
-
-if [[ -z $MY_FILE]] ; then
-    usage
-    exit 1
-fi
-
-if [[ -z $SI_UBL_VERSION ]] ; then
-    usage
-    exit 1
-fi
-
-
-
 function do_cmd {
     echo $@
     $@
@@ -67,7 +11,7 @@ if [ $1 -eq 0 ] ; then
 else  
     echo "[ERROR] $2"
     if [ $QUIT -eq 1 ] ; then
-        echo java -jar $XTST_JAR_FILE -f $i $XSL_DIR/SI-UBL-$SI_UBL_VERSION.xsl /opt/xsd/UBL-Order-2.1.xsd   
+        echo "This command failed."
         exit 1 
     fi
 fi 
@@ -79,34 +23,39 @@ if [ $1 -eq 1 ] ; then
 else 
     echo "[ERROR] $2"
     if [ $QUIT -eq 1 ] ; then
-        echo "This command failed: java -jar $XTST_JAR_FILE -f $i $XSL_FILE /opt/xsd/maindoc/UBL-Invoice-2.1.xsd" 
+        echo "This command failed."
         exit 1 
     fi
 fi 
 }
 
-
-#################################################################
-# Scan testset for file with ok,warning and error and process them. 
-# if the output contains validation failures this is either nok (for valid SI-UBL documents)
-# or ok (for warnings and errors)
-# LATER: check weither the exepcted rule is executed or not.
+QUIT=1
+TESTSET_SI_UBL_VERSION=1.1
+SI_UBL_VERSION=1.1
+XML_FILES=/opt/testset
+XSL_DIR=/opt/si-ubl
 
 if  [ $TESTSET_SI_UBL_VERSION = 1.1 ]; then
     for i in $XML_FILES/SI-UBL-$TESTSET_SI_UBL_VERSION/SI-UBL-$TESTSET_SI_UBL_VERSION-ok-*.xml; do
-        do_cmd java -jar $XTST_JAR_FILE -f $i $XSL_DIR/SI-UBL-INV-$SI_UBL_VERSION.xsl /opt/xsd/maindoc/UBL-Invoice-2.1.xsd  | grep fail
+        do_cmd java -cp /opt/java-tools/en16931-xml-validator.jar eu.cen.en16931.xmlvalidator.XMLValidator \
+         -xml $i -xslt $XSL_DIR/SI-UBL-INV-$SI_UBL_VERSION.xsl
+         cat result.svrl  | grep fail
         # grep fails when not found. so 1=ok 0=nok
         do_check_inverse $? $i
     done
     
-    for i in $XML_FILES/SI-UBL-$TESTSET_SI_UBL_VERSION/SI-UBL-$TESTSET_SI_UBL_VERSION-warning-*.xml; do
-        do_cmd java -jar $XTST_JAR_FILE -f $i $XSL_DIR/SI-UBL-INV-$SI_UBL_VERSION.xsl  | grep flag=\"warnin\"
-        # grep fails when not found. so 0=ok 1=nok
-        do_check $? $i
-    done
+    # for i in $XML_FILES/SI-UBL-$TESTSET_SI_UBL_VERSION/SI-UBL-$TESTSET_SI_UBL_VERSION-warning-*.xml; do
+    #     do_cmd java -cp /opt/java-tools/en16931-xml-validator.jar eu.cen.en16931.xmlvalidator.XMLValidator \
+    #      -xml $i -xslt $XSL_DIR/SI-UBL-INV-$SI_UBL_VERSION.xsl
+    #     cat result.svrl  | grep flag=\"warnin\"
+    #     # grep fails when not found. so 0=ok 1=nok
+    #     do_check $? $i
+    # done
 
     for i in $XML_FILES/SI-UBL-$TESTSET_SI_UBL_VERSION/SI-UBL-$TESTSET_SI_UBL_VERSION-error-*.xml; do
-        do_cmd java -jar $XTST_JAR_FILE -f $i $XSL_DIR/SI-UBL-INV-$SI_UBL_VERSION.xsl | grep fail
+        do_cmd java -cp /opt/java-tools/en16931-xml-validator.jar eu.cen.en16931.xmlvalidator.XMLValidator \
+         -xml $i -xslt $XSL_DIR/SI-UBL-INV-$SI_UBL_VERSION.xsl
+         cat result.svrl  | grep fail
         # grep fails when not found. so 0=ok 1=nok
        do_check $? $i
     done
@@ -114,28 +63,32 @@ fi
 
 if  [ $TESTSET_SI_UBL_VERSION = 1.2 ]; then
     for i in $XML_FILES/SI-UBL-$TESTSET_SI_UBL_VERSION/SI-UBL-INV-$TESTSET_SI_UBL_VERSION-ok-*.xml; do
-        do_cmd java -jar $XTST_JAR_FILE -f $i $XSL_DIR/SI-UBL-INV-$SI_UBL_VERSION.xsl /opt/xsd/maindoc/UBL-Invoice-2.1.xsd  | grep fail
+        do_cmd java -cp /opt/java-tools/en16931-xml-validator.jar eu.cen.en16931.xmlvalidator.XMLValidator \
+         -xml $i -xslt $XSL_DIR/SI-UBL-INV-$SI_UBL_VERSION.xsl
+         cat result.svrl  | grep fail
         # grep fails when not found. so 1=ok 0=nok
         do_check_inverse $? $i
     done
 
     for i in $XML_FILES/SI-UBL-$TESTSET_SI_UBL_VERSION/SI-UBL-INV-$TESTSET_SI_UBL_VERSION-error-*.xml; do
-        do_cmd java -jar $XTST_JAR_FILE -f $i $XSL_DIR/SI-UBL-INV-$SI_UBL_VERSION.xsl | grep fail
-        # grep fails when not found. so 0=ok 1=nok
+        do_cmd java -cp /opt/java-tools/en16931-xml-validator.jar eu.cen.en16931.xmlvalidator.XMLValidator \
+         -xml $i -xslt $XSL_DIR/SI-UBL-INV-$SI_UBL_VERSION.xsl
+         cat result.svrl  | grep fail
+              # grep fails when not found. so 0=ok 1=nok
        do_check $? $i
     done
 
-    for i in $XML_FILES/SI-UBL-$TESTSET_SI_UBL_VERSION/SI-UBL-PO-$TESTSET_SI_UBL_VERSION-ok-*.xml; do
-        do_cmd java -jar $XTST_JAR_FILE -f $i $XSL_DIR/SI-UBL-PO-$SI_UBL_VERSION.xsl /opt/xsd/maindoc/UBL-Invoice-2.1.xsd  | grep fail
-        # grep fails when not found. so 1=ok 0=nok
-        do_check_inverse $? $i
-    done
+    # for i in $XML_FILES/SI-UBL-$TESTSET_SI_UBL_VERSION/SI-UBL-PO-$TESTSET_SI_UBL_VERSION-ok-*.xml; do
+    #     do_cmd java -jar $XTST_JAR_FILE -f $i $XSL_DIR/SI-UBL-PO-$SI_UBL_VERSION.xsl /opt/xsd/maindoc/UBL-Invoice-2.1.xsd  | grep fail
+    #     # grep fails when not found. so 1=ok 0=nok
+    #     do_check_inverse $? $i
+    # done
 
-    for i in $XML_FILES/SI-UBL-$TESTSET_SI_UBL_VERSION/SI-UBL-PO-$TESTSET_SI_UBL_VERSION-error-*.xml; do
-        do_cmd java -jar $XTST_JAR_FILE -f $i $XSL_DIR/SI-UBL-PO-$SI_UBL_VERSION.xsl | grep fail
-        # grep fails when not found. so 0=ok 1=nok
-       do_check $? $i
-    done
+    # for i in $XML_FILES/SI-UBL-$TESTSET_SI_UBL_VERSION/SI-UBL-PO-$TESTSET_SI_UBL_VERSION-error-*.xml; do
+    #     do_cmd java -jar $XTST_JAR_FILE -f $i $XSL_DIR/SI-UBL-PO-$SI_UBL_VERSION.xsl | grep fail
+    #     # grep fails when not found. so 0=ok 1=nok
+    #    do_check $? $i
+    # done
 fi
 
 
